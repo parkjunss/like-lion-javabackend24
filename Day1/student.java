f316550de4b165170d3e7e744d98da69447b01b5
parkjunss <wnstjd117@gmail.com>
2026-04-02 09:27:14 +0900

    first commit

---
+++ Day1/student.java
@@ -0,0 +1,7 @@
+public class student {
+    public static void main(String[] args) {
+        System.out.println("학생 정보");
+        System.out.println("이름: 홍길동");
+        System.out.println("나이: 20");
+    }
+}
